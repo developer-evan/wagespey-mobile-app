c8be35a8b1790be1ce7313a05c2b0a3212efd91f
Mogeni <evansmogeni254@gmail.com>
2024-06-22 17:05:14 +0300

    base setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent:'center', alignItems:'center', backgroundColor:'#daf' }}>
       <Text>Home Screen</Text>
+      <HelloWave />
     </View>
 
     
